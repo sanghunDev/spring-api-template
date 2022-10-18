15e877f76817bc273e970f167dd64cff95ad0ea5
sanghunDev <bsh20057@gmail.com>
2022-10-18 17:42:19 +0900

    perf(cors): cors 설정 - cors 허용 설정 로직 작성

---
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -0,0 +1,25 @@
+package com.app.global.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    //cors 허용
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")              //허용 url 설정
+                .allowedOrigins("http://localhost:8082")      // 허용 origins (* : 모두 , 여러개는 , 로 구분해서 설정 가능)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                );  //허용할 method 목록
+    }
+}
