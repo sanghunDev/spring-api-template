1cb1966763f25248170ba27ca93b43e320a745c8
sanghun <bsh20057@gmail.com>
2022-10-24 00:04:20 +0900

    perf(회원): 회원 엔티티 필드 추가 - 회원에 대한 기본 정보 필드 추가

--- src/main/java/com/app/domain/member/entity/Member.java
+++ src/main/java/com/app/domain/member/entity/Member.java
@@ -19,4 +19,16 @@ public class Member {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 10)
     private MemberType memberType;
+
+    @Column(unique = true, length = 50, nullable = false)
+    private String email;
+
+    @Column(length = 200)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String memberName;
+
+    @Column(length = 200)
+    private String profile;
 }
