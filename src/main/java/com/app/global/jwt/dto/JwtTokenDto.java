180081ce06d70b1d9b597864a34c228d77ebcdb3
sanghunDev <bsh20057@gmail.com>
2022-10-26 11:26:17 +0900

    perf(jwt): JwtTokenDto 날짜 포맷 - 만료시간 날짜 포맷 변경

--- src/main/java/com/app/global/jwt/dto/JwtTokenDto.java
+++ src/main/java/com/app/global/jwt/dto/JwtTokenDto.java
@@ -1,5 +1,6 @@
 package com.app.global.jwt.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.util.Date;
@@ -13,7 +14,9 @@ public class JwtTokenDto {
 
     private String grantType;
     private String accessToken;
-    private Date accessTokenExpireTime;
     private String refreshToken;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private Date accessTokenExpireTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Date refreshTokenExpireTime;
 }
