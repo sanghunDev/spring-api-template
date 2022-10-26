20ac46b951789e90d9c4401ccb9cb713ad4017f2
sanghunDev <bsh20057@gmail.com>
2022-10-26 19:42:04 +0900

    perf(소셜 로그인): 반환 클래스 - 소셜 로그인에 대한 반환 클래스 추가

---
+++ src/main/java/com/app/api/login/dto/OauthLoginDto.java
@@ -0,0 +1,28 @@
+package com.app.api.login.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.util.Date;
+
+public class OauthLoginDto {
+
+    @Getter
+    @Setter
+    public static class Request {
+        private String memberType;
+    }
+
+    @Getter
+    @Setter
+    @Builder @NoArgsConstructor @AllArgsConstructor
+    public static class Response {
+        private String grantType;
+        private String accessToken;
+        private String refreshToken;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private Date accessTokenExpireTime;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private Date refreshTokenExpireTime;
+    }
+}
