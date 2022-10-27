8d1c9ac9141476216de26f65179f93aa00d192dc
sanghunDev <bsh20057@gmail.com>
2022-10-27 10:09:10 +0900

    perf(error): 회원 에러 코드 추가 - 회원을 찾을 수 없을 때 사용할 에러코드 추가

--- src/main/java/com/app/global/error/ErrorCode.java
+++ src/main/java/com/app/global/error/ErrorCode.java
@@ -16,11 +16,13 @@ public enum ErrorCode {
     NOT_EXISTS_AUTHORIZATION(HttpStatus.UNAUTHORIZED, "A-003", "Authorization Header가 빈 값 입니다."), 
     NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "A-004", "인증 타입이 Bearer 타입이 아닙니다"),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A-005", "해당 refresh token 이 존재하지 않습니다."),
-    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-005", "해당 refresh token은 만료 되었습니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-006", "해당 refresh token은 만료 되었습니다."),
+    NOT_ACCESS_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "A-007", "해당 토큰은 access token이 아닙니다."),
 
     //회원
     INVALID_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "M-001","잘못된 회원 타입 입니다" ),
-    ALREADY_REGISTERED_MEMBER(HttpStatus.BAD_REQUEST, "M-002", "이미 가입된 회원 입니다.");
+    ALREADY_REGISTERED_MEMBER(HttpStatus.BAD_REQUEST, "M-002", "이미 가입된 회원 입니다."),
+    MEMBER_NOT_EXISTS(HttpStatus.BAD_REQUEST, "M-003", "해당 회원은 존재하지 않습니다");
 
     ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
         this.httpStatus = httpStatus;
