ef7eb588b9bc36857ac686b940a89ef880a875bc
sanghunDev <bsh20057@gmail.com>
2022-10-27 10:58:21 +0900

    perf(api): 회원 조회 - 회원 조회 api 추가

---
+++ src/main/java/com/app/api/member/controller/MemberIntoController.java
@@ -0,0 +1,32 @@
+package com.app.api.member.controller;
+
+import com.app.api.member.dto.MemberInfoResponseDto;
+import com.app.api.member.service.MemberInfoService;
+import com.app.global.jwt.service.TokenManager;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberIntoController {
+
+    private final MemberInfoService memberInfoService;
+    private final TokenManager tokenManager;
+
+    @GetMapping("/info")
+    public ResponseEntity<MemberInfoResponseDto> getMemberInfo(@RequestHeader("Authorization") String authorizationHeader) {
+
+        String accessToken = authorizationHeader.split(" ")[1];
+        Claims tokenClaims = tokenManager.getTokenClaims(accessToken);
+        Long memberId = Long.valueOf((Integer)tokenClaims.get("memberId"));
+        MemberInfoResponseDto memberInfoDto = memberInfoService.getMemberInfo(memberId);
+
+        return ResponseEntity.ok(memberInfoDto);
+    }
+}
