ebf6cd80b60baf5e340385d18d0d5943ae2481df
sanghunDev <bsh20057@gmail.com>
2022-10-27 10:29:54 +0900

    perf(config): 인터셉터 설정 추가 - 인증 인터셉터 등록 - 인증 인터셉터 설정( 순서 1번, /api/** 하위로 오는 모든 요청에 적용, 인증을 제외할 url 설정)

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,12 +1,18 @@
 package com.app.global.config.web;
 
+import com.app.global.intercepter.AuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private AuthenticationInterceptor authenticationInterceptor;
 
     //cors 허용
     @Override
@@ -22,5 +28,17 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.OPTIONS.name()
                 )                                               //허용할 method 목록
                 .maxAge(3600);                                  //프리플라이트 재요청 시간 변경
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .order(1)                   //인터셉터 실행순서 (인증이 가장 먼저 동작)
+                .addPathPatterns("/api/**") //어떤 요청에 적용시킬건가
+                .excludePathPatterns(
+                        "/api/oauth/login",
+                        "/api/access-token/issue",
+                        "/api/logout",
+                        "/api/health");      //인터셉터 적용 예외 요청
     }
 }
