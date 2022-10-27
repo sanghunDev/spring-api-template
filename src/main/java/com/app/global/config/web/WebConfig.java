a6605a54c60fc2d44206df03c667c02edb79d737
sanghunDev <bsh20057@gmail.com>
2022-10-27 11:16:21 +0900

    perf(config): resolver 추가 - memberInfoArgumentResolver 사용 가능하도록 추가

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,18 +1,23 @@
 package com.app.global.config.web;
 
 import com.app.global.intercepter.AuthenticationInterceptor;
+import com.app.global.resolver.memberInfo.MemberInfoArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthenticationInterceptor authenticationInterceptor;
+    private final MemberInfoArgumentResolver memberInfoArgumentResolver;
 
     //cors 허용
     @Override
@@ -40,5 +45,10 @@ public class WebConfig implements WebMvcConfigurer {
                         "/api/access-token/issue",
                         "/api/logout",
                         "/api/health");      //인터셉터 적용 예외 요청
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberInfoArgumentResolver);
     }
 }
