1392830b1ebfcef13d34619c4e61f9d65f60a542
sanghunDev <bsh20057@gmail.com>
2022-10-27 09:40:21 +0900

    perf(error): token 관련 에러 추가 - refresh token 관련 에러 코드 추가

--- src/main/java/com/app/global/error/ErrorCode.java
+++ src/main/java/com/app/global/error/ErrorCode.java
@@ -15,6 +15,8 @@ public enum ErrorCode {
     NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "A-002", "해당 토큰은 유효한 토큰이 아닙니다."),
     NOT_EXISTS_AUTHORIZATION(HttpStatus.UNAUTHORIZED, "A-003", "Authorization Header가 빈 값 입니다."), 
     NOT_VALID_BEARER_GRANT_TYPE(HttpStatus.UNAUTHORIZED, "A-004", "인증 타입이 Bearer 타입이 아닙니다"),
+    REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A-005", "해당 refresh token 이 존재하지 않습니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-005", "해당 refresh token은 만료 되었습니다."),
 
     //회원
     INVALID_MEMBER_TYPE(HttpStatus.BAD_REQUEST, "M-001","잘못된 회원 타입 입니다" ),
