2f823a314bde2ea8b8ce22438f756cb6cc8204a1
sanghun <bsh20057@gmail.com>
2022-10-29 23:34:14 +0900

    perf(swagger): 제목 설졍 - swagger 문서 제목 및 기본설명 설정

--- src/main/java/com/app/global/config/SwaggerConfig.java
+++ src/main/java/com/app/global/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.app.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,6 +18,16 @@ public class SwaggerConfig {
                 .select()   // ApiSelectorBuilder 생성
                 .apis(RequestHandlerSelectors.basePackage("com.app.api"))   // API 패키지 경로 todo 패키지 경로 수정
                 .paths(PathSelectors.ant("/api/**"))     //any: 모든 api 경로에 대해 문서화, ant: path 조건에 따라서 API 문서화
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API 문서")
+                .description("API에 대해 설명해주는 API 문서입니다")
+                .version("1.0")
                 .build();
     }
+
 }
