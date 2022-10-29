1635c46ae80367a3a16827c21aee4e4b63324151
sanghun <bsh20057@gmail.com>
2022-10-29 23:48:33 +0900

    perf(swagger): Authorization 사용 - Authorization 사용하여 jwt 인증 가능하게 처리

--- src/main/java/com/app/global/config/SwaggerConfig.java
+++ src/main/java/com/app/global/config/SwaggerConfig.java
@@ -1,13 +1,21 @@
 package com.app.global.config;
 
+import com.app.global.resolver.memberInfo.MemberInfo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -20,7 +28,10 @@ public class SwaggerConfig {
                 .paths(PathSelectors.ant("/api/**"))     //any: 모든 api 경로에 대해 문서화, ant: path 조건에 따라서 API 문서화
                 .build()
                 .apiInfo(apiInfo())
-                .useDefaultResponseMessages(false);
+                .useDefaultResponseMessages(false)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .ignoredParameterTypes(MemberInfo.class);
     }
 
     private ApiInfo apiInfo() {
@@ -31,4 +42,20 @@ public class SwaggerConfig {
                 .build();
     }
 
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
 }
